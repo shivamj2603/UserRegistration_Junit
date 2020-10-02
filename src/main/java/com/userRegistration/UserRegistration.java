8f5a8fc609cc0adf399f3751c81dfae9e59c3de6
shivamj2603 <shivamj2603@gmail.com>
2020-10-03 03:00:53 +0530

    Refactored:Improved code structure

--- src/main/java/com/userRegistration/UserRegistration.java
+++ src/main/java/com/userRegistration/UserRegistration.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class UserRegistration {
 	public static boolean validateInput(String input,String expression)
 	{
